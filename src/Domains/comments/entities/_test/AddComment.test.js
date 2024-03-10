7acee76dfd923cf09a8f9725b78cc43dd90061b6
Ayi Akbar <icamaulana2612@gmail.com>
2024-03-10 10:03:49 +0700

    add eslint-disable no-unused-vars on every domain repository

--- src/Domains/comments/entities/_test/AddComment.test.js
+++ src/Domains/comments/entities/_test/AddComment.test.js
@@ -8,4 +8,29 @@ describe('AddComment entity', () => {
     // Action & Assert
     expect(() => new AddComment(payload)).toThrowError('ADD_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
   });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      content: true,
+    };
+
+    // Action & Assert
+    expect(() => new AddComment(payload)).toThrowError(
+      'ADD_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION'
+    );
+  });
+
+  it('should create addComment object correctly', () => {
+    // Arrange
+    const payload = {
+      content: 'siapa yang mau beli cilok?',
+    };
+
+    // Action
+    const { content } = new AddComment(payload);
+
+    // Assert
+    expect(content).toEqual(payload.content);
+  });
 });
